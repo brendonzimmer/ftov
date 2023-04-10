e79520ef4b54ce54ef0adeb44f3b2b7191b59bf1
Brendon Zimmer <bzimmer@usc.edu>
2023-04-10 10:37:08 -0700

    Try 2 bit colors to video

---
+++ src/encode.rs
@@ -0,0 +1,49 @@
+use std::{process::{Command, Stdio}, io::Write, path::PathBuf};
+
+// 1 square == 3x3 pixels -> 2 bytes
+pub const FPS: usize = 30;
+pub const SQUARE: usize = 10;
+pub const HEIGHT: usize = 10; // 2180;
+
+pub const WIDTH: usize = 10; // 3840;
+pub const BWIDTH: usize = WIDTH*3;
+pub const UWIDTH: usize = WIDTH*2/SQUARE;
+
+pub const UFRAME: usize = UWIDTH*HEIGHT/SQUARE;
+
+pub fn encode(data: &mut Vec<[u8; BWIDTH]>, output: PathBuf) {
+    let mut ffmpeg = Command::new("ffmpeg");
+
+    ffmpeg.args(&[
+        "-f", "rawvideo",
+        "-pix_fmt", "rgb24",
+        "-s", &format!("{WIDTH}x{HEIGHT}"),
+        "-r", &FPS.to_string(),
+        "-i", "-",
+        "-c:v", "libx264",
+        "-crf", "0",
+        // "-threads", "9",
+        &format!("{}", output.to_str().unwrap()),
+    ]).stdin(Stdio::piped()).stdout(Stdio::null()).stderr(Stdio::inherit());
+    
+    let mut run = ffmpeg.spawn().unwrap();
+    
+    let stdin = run.stdin.as_mut().unwrap();
+    
+    // flatten the 2D array into a 1D array
+    let mut h = data.len();
+    
+    stdin.write_all(&data.iter().flatten().copied().collect::<Vec<u8>>()).unwrap();
+    while h < HEIGHT {
+        stdin.write_all(&[0u8; BWIDTH]).unwrap();
+        h += 1;
+    }
+    
+    
+    stdin.flush().unwrap();
+    drop(stdin);  // Close the pipe to signal the end of input
+    
+    // Wait for ffmpeg to finish encoding the video
+    let status = run.wait().unwrap();
+    assert!(status.success());
+}
---
+++ src/main.rs
@@ -0,0 +1,90 @@
+mod encode;
+
+use crate::{encode::{encode, UWIDTH, BWIDTH, SQUARE, WIDTH}};
+use std::{path::{PathBuf, Path}, io::{BufReader, self, Read}, fs::File};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, value_name = "FILEPATH")]
+    input: PathBuf,
+
+    #[arg(short, long, value_name = "PATH", default_value = Option::None)]
+    output: Option<PathBuf>
+}
+
+fn main() -> io::Result<()> {
+    let Args {input, output} = Args::parse();
+    let output = output.unwrap_or_else(|| Path::new("./").with_file_name(input.file_name().unwrap()).with_extension("mp4"));
+
+    if input.is_dir() { 
+        panic!("Input must be a file, not a directory")
+    }
+
+    let mut frame = vec![[0u8; BWIDTH]];
+    for _ in 0..SQUARE-1 {
+        frame.push([0u8; BWIDTH]);
+    }
+
+    let mut w: usize = 0;
+    let mut h: usize = 0;
+
+    let mut reader = BufReader::new(File::open(input)?);
+    let mut buf = [0u8; UWIDTH];
+    loop {
+        println!("starting loop");
+        match reader.read(&mut buf) {
+            Ok(0) => {println!("breaking"); break;},
+            Ok(num) => {
+                let iter = buf[..num].chunks_exact(2);
+                
+                iter.clone().for_each(|pair| {
+                    for wd in 0..SQUARE {
+                        for hd in 0..SQUARE { 
+                            if frame[h+hd][w+1+(wd*3)] != 0 || frame[h+hd][w+2+(wd*3)] != 0 {
+                                panic!("Overwriting non-zero value");
+                            } 
+                            println!("Writing {:?}", pair);
+                            frame[h+hd][w+1+(wd*3)] = pair[0];
+                            frame[h+hd][w+2+(wd*3)] = pair[1];
+                        }
+                    }
+                    w += (SQUARE)*3;
+                });
+
+                if let Some(byte) = iter.remainder().first() {
+                    for wd in 0..SQUARE {
+                        for hd in 0..SQUARE { 
+                            if frame[h+hd][w+1+(wd*3)] != 0 || frame[h+hd][w+2+(wd*3)] != 0 {
+                                panic!("Overwriting non-zero value");
+                            } 
+                            println!("Writing 1 byte: {}", byte);
+                            frame[h+hd][w+1+(wd*3)] = *byte;
+                        }
+                    }
+                }
+
+                w = 0;
+                h += SQUARE;
+                
+                for _ in 0..SQUARE {
+                    frame.push([0u8; BWIDTH]);
+                }
+                println!("{} bytes read", num);
+            },
+            Err(e) => panic!("Error reading file: {}", e)
+        };
+    }
+
+    let size = frame.len();
+    for wd in 0..SQUARE {
+        for hd in 0..SQUARE { 
+            frame[hd][(wd*3)] = 5;
+            frame[hd+(size-SQUARE)][(WIDTH-SQUARE+(wd*3))] = 255;
+        }
+    }
+    
+    encode(&mut frame, output);
+
+    Ok(())
+}
